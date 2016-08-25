73a93c2ef01ca9ca3b28424c7d9229487e2e7c91
VL MS <vl0ms@users.noreply.github.com>
2016-08-25 12:47:57 +0300

    Add files via upload

---
+++ rtorrent/runrtorrent.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Check lock
+if [ -f /rtorrent/.session/rtorrent.lock ]; then
+    rm -f /rtorrent/.session/rtorrent.lock
+fi
+
+# Check config
+if [ ! -f /rtorrent/.rtorrent.rc ]; then
+    cp /usr/local/src/.rtorrent.rc /rtorrent/.rtorrent.rc
+fi
+
+# change listening port if env $RTORRENT_PORT is set
+
+if [ "$RTORRENT_PORT" ]; then
+    sed -i "s/^port_range.*$/port_range=${RTORRENT_PORT}-${RTORRENT_PORT}/" /rtorrent/.rtorrent.rc
+fi
+
+# change listening port if env $RTORRENT_DHT is set
+
+if [ "$RTORRENT_DHT" ]; then
+    sed -i "s/^dht_port.*$/dht_port=${RTORRENT_DHT}/" /rtorrent/.rtorrent.rc
+fi
+
+# Run rTorrent 
+exec /usr/local/bin/rtorrent
